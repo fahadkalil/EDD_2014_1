6475f013c6e699a5d5cf7ae6b39e6cc575bc0e19
fahad <fahadkalil@gmail.com>
2014-03-11 19:00:51 -0300

    Aula 01

---
+++ EDD_2014_1/src/aula01/Aluno.java
@@ -0,0 +1,90 @@
+package aula01;
+
+public class Aluno {
+	private String nome;
+	private String email;
+	private int idade;
+	private boolean casado;
+	
+	public Aluno(String nome, String email, int idade, boolean casado) {
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.casado = casado;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public boolean isCasado() {
+		return casado;
+	}
+	public void setCasado(boolean casado) {
+		this.casado = casado;
+	}
+	
+	public static void main(String[] args) {
+		// Cria objeto		
+		Aluno aluno1 = new Aluno("Pedro","pedro@imed.edu.br", 25, false);
+		
+		// Mostra na tela
+		System.out.println( "Nome do aluno: " + aluno1.getNome() );
+		
+		// Cria objeto
+		Aluno aluno2 = new Aluno("João","joao@imed.edu.br", 45, true);
+		
+		// Cria um vetor de Aluno
+		Aluno[] alunos = new Aluno[5];
+		alunos[0] = aluno1;
+		alunos[1] = aluno2;
+		
+		
+		// Percorrer vetor		
+		for (Aluno a : alunos) {
+			if (a != null) {
+				System.out.println( "Nome do aluno: " + a.getNome() );
+			}
+		}
+		
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
